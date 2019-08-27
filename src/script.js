8a03fad84726a62c812650e94baef98ab9731b4f
git <v.kundryukov@gmail.com>
2019-08-27 19:38:28 +0300

    lesson 03 base

--- src/script.js
+++ src/script.js
@@ -1,36 +1,81 @@
+'use strict';
 
-// объявим переменнные
-let money = 301,
-    income = '150000',
-    addExpenses = 'Квартира - 55000,Ресторан - 20000, Такси - 15000';
-    deposit = false,
-    mission = 45000000,
-    period = 4;
+// дополнительный доход
+let income = 0; 
 
-// выведем типы данных в консоль
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+// сумма необходимого накопления
+let mission = 400000;
 
-// выведем длину строки 
-console.log(income.length);
+// доход за месяц
+let money = prompt('Ваш месячный доход?', '50000');
 
-// выведем значения в тексте переменных period и mission в консоль 
-console.log(String.raw`Период ${period} месяцев и Цель заработать ${mission} долларов'`);
+// дополнительный расход    
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 
+                         'оружие, наркотики, шлюхи');
+console.log(addExpenses.split(','));
 
-// переведем в нижний регистр и разобъем на массив
-console.log( String( addExpenses.toLowerCase() ).split( ',' ) );
+// объявим депозит и спросим про его наличие
+let deposit = prompt('Есть ли у вас депозит в банке?', 'нет я бомж') == 'да' ? true : false;
+console.log('deposit - ',deposit); 
 
-// вывод результата и остатка от деления
-let budgetDay = money / 30;
-// проверим целое число или нет
-if (!Number.isSafeInteger(budgetDay)) {
-    // если дробное, разобъем на массив и выведем отдельно целую и дробную часть
-    console.log(
-                'Результат:', String(budgetDay).split('.')[0], 
-                'Остаток:', String(budgetDay).split('.')[1]
-                );
-} else {
-    // иначе выведем просто число
-    console.log('Результат:', budgetDay);
+// Вывести в консоль типы данных money, income, deposit
+console.log('Тип money - ', typeof money);
+console.log('Тип addExpenses - ', typeof addExpenses);
+console.log('Тип income - ', typeof income);
+
+// возврат значения ввиде массива вопрос / ответ
+function getArrayRecord(questionOne, questionTwo, baseValueOne = '1', baseValueTwo = '200') {
+    return [prompt(questionOne, baseValueOne), prompt(questionTwo, baseValueTwo)];
 }
+
+// доп расходы 
+// объявление переменных
+let question1 = 'Какие обязательные ежемесячные расходы у вас есть?';
+let question2 = 'Во сколько это обойдется?';
+let qountQuestion = 2; // кол-во запросов по расходам 
+let additionalСost = new Array(); // расходы
+
+// cпросим про расходы и добавим в массив 
+for (let index = 0; index < qountQuestion; index++) {
+    additionalСost.push( getArrayRecord(question1, question2) );    
+}
+
+// вычислим доходы за месяц
+let allExpense = 0;
+
+additionalСost.forEach((element) => {
+    allExpense += Number(element[1]);
+});
+
+let budgetMonth = money - allExpense;
+console.log('Доход за месяц:', budgetMonth);
+
+// расчет кол-ва месяцев для достижения фин. цели 
+let missionMonthCount = mission / budgetMonth;
+console.log('Достижение цели', mission,'за', Math.ceil( missionMonthCount), 'мес.');
+
+// вычисления бюджета на день
+let dayInMonth = 30;
+let budgetDay = money / dayInMonth;
+console.log('Дневной бюджет:', Math.floor(budgetDay));
+
+// определим уровень дохода за день
+switch (true) {
+    
+    case (budgetDay > 800):
+        console.log('Высокий уровень дохода');
+        break;
+    
+    case (budgetDay > 300 && budgetDay < 800):
+        console.log('Средний уровень дохода');
+        break;
+
+    case (budgetDay > 0 && budgetDay < 300):
+        console.log('Низкий уровень дохода');
+        break;
+
+    case (budgetDay < 0):
+        console.log('Что-то пошло не так');
+        break;
+
+};
