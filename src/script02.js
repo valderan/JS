bdce70da4e40eac3d3937023c220bba682244e27
git <v.kundryukov@gmail.com>
2019-08-28 18:05:33 +0300

    lesson 04 hard

--- src/script02.js
+++ src/script02.js
@@ -1,70 +1,23 @@
 'use strict';
 
-// зададим дни недели в 2х локалях
-let rusDays = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье',
-    engDays = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
+// тестовая строка 32 символа
+let testString = '12345678901234567890123456789012';
 
-// запросим локаль, по умслчанию установка в ru
-let lang = prompt('Локализация только:ru/en | Localization only:ru/en', 'ru').toLowerCase();
-
-// проверим на соблюдения условия
-switch (lang) {
-    case 'en':
-    case 'eng':    
-    case 'english':  
-        lang = 'en'; 
-        break;
-    default:
-        lang = 'ru';
-        break;
+// функция обрезания строки более 30 символов
+let shortString = function(string) {
+    let workStr = String(string).trim();
+    if (workStr.length > 30) {
+        return workStr.substr(0,30) + '...';
+    } else {
+        return workStr;
+    }
 };
 
-// выведем дни недели через if
-if (lang == 'en') {
-    console.log(engDays);
-} else {
-    console.log(rusDays);
+// проверим передаваемый параметр 
+let test = function(data) {
+    if (typeof(data) === 'function') {
+        console.log('Параметр data - функция');
+    }   
 };
 
-// выведем через switch - case
-switch(lang) {
-    
-    case 'en':
-        console.log(engDays);
-        break;
-    
-    default:
-        console.log(rusDays);
-        break;
-};
-
-//при помощи многомерных массивов   
-// заполним массив 
-let locale = 
-    [   
-        [
-            'en',
-            engDays
-        ],
-        [
-            'ru',
-            rusDays
-        ]
-
-    ]
-
-// так как у нас четко заданы массивы и занчечения lang проверяются, 
-// мы можем не боясь фильтровать и на прямую обращаться к элементу
-// но реализация через хэщ была бы изящнее, но я так понял надо через многомерные массивы
-let localeString = locale.filter(element => element[0] == lang)[0][1];
-
-console.log(localeString);
-
- // работа с Именем -> должностью
-let namePerson = 'Артем',
-    printString = 'студент';
-     
-(namePerson == 'Артем') ? printString = 'директор' : printString;
-(namePerson == 'Максим') ? printString = 'преподаватель' : printString;
-
-console.log(printString);
+test(shortString);
