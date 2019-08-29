4bda8283b8b5f4a8ebbbef32ec81bb1c9edc82e1
git <v.kundryukov@gmail.com>
2019-08-29 10:46:32 +0300

    lesson 05 base

--- src/script.js
+++ src/script.js
@@ -7,7 +7,17 @@ let income = 0;
 let mission = 400000;
 
 // доход за месяц
-let money = prompt('Ваш месячный доход?', '50000');
+let money;
+
+// ввод и проверка суммы ежемесячного дохода
+let start = function() {
+    money = prompt('Ваш месячный доход?', '50000');
+
+    while (isNaN(money) || money === '' || money === null) {
+        money = prompt('Ваш месячный доход?', '50000');
+    }
+
+}
 
 // возврат значения ввиде массива вопрос / ответ
 let getArrayRecord = function(questionOne, questionTwo, baseValueOne = '1', baseValueTwo = '200') {
@@ -18,29 +28,42 @@ let getArrayRecord = function(questionOne, questionTwo, baseValueOne = '1', base
 let showTypeOf = function(data) {
     console.log(data, typeof(data));
 };
-// доп расходы 
-// объявление переменных
-let question1 = 'Какие обязательные ежемесячные расходы у вас есть?';
-let question2 = 'Во сколько это обойдется?';
-let qountQuestion = 2; // кол-во запросов по расходам 
-let additionalСost = new Array(); // расходы
 
-// cпросим про расходы и добавим в массив 
-for (let index = 0; index < qountQuestion; index++) {
-    additionalСost.push( getArrayRecord(question1, question2) );    
-}
+// расходы
+let additionalСost = new Array(); 
 
-// расчитаем доходы за месяц 
-let accumulatedMonth = getAccumulatedMonth(getExpensesMonth(additionalСost), money);
-console.log('Доход за месяц:', accumulatedMonth);
+/*
+Добавление/удаление расходов
+    costs(array) - массив расходов;
+    count(int) - кол-во операций добавления;
+    reload(boolean) - флаг очистки: 
+            false - добавление в конец существующего массива 
+            true - очистка массива перед добавлением
+*/
+let setExpenses = function(costs, count = 2, reload = false) {
 
-// расчет кол-ва месяцев для достижения фин. цели 
-let missionMonthCount = getTargetMonth(mission, accumulatedMonth);
-console.log('Достижение цели', mission,'за', Math.floor(missionMonthCount), 'мес.');
+    let question1 = 'Какие обязательные ежемесячные расходы у вас есть?';
+    let question2 = 'Во сколько это обойдется?';
+    let qountQuestion = count; // кол-во запросов по расходам 
 
+    // reload === true очистка массива перед добавлением
+    if (reload) {
+        let countDelElem = costs.length;
+        costs.splice(0, countDelElem);
+    }
+
+    // cпросим про расходы и добавим в массив 
+    for (let index = 0; index < qountQuestion; index++) {
+        costs.push( getArrayRecord(question1, question2) );    
+    }
+
+};
 
 // расчет всех расходов 
 function getExpensesMonth(expenses) {
+
+    // зададим дополнительные расходы
+    setExpenses(expenses);
 
     let allExpense = 0;
 
@@ -63,12 +86,9 @@ function  getTargetMonth(missionMonth, monthBudget) {
     return missionMonth / monthBudget;
 };
 
-// вычисления бюджета на день
-function getStatusIncome(money) {
-
-    let dayInMonth = 30;
-    let budgetDay = money / dayInMonth;    
-
+// информация об уровне дохода
+function getStatusIncome(budgetDay) {
+  
     switch (true) {
     
         case (budgetDay >= 800):
@@ -79,13 +99,46 @@ function getStatusIncome(money) {
             return 'Средний уровень дохода';
             break;
 
-        case (budgetDay > 0 && budgetDay < 300):
+        case (budgetDay >= 0 && budgetDay < 300):
             return 'Низкий уровень дохода';
             break;
 
-        case (budgetDay <= 0):
+        case (budgetDay < 0):
             return 'Что-то пошло не так';
             break;
 
     };
 };
+
+/*
+    Вычисление суммы зароботка в день
+    money - месячный доход 
+    dayInMonth - кол-во дней в месяце 
+*/
+function getBudgetDay(money, dayInMonth = 30) {
+    return money / dayInMonth;
+}
+
+
+// зададим сумму ежемесячного дохода
+start();
+
+// расчитаем доходы за месяц 
+let accumulatedMonth = getAccumulatedMonth(getExpensesMonth(additionalСost), money);
+console.log('Доход за месяц:', accumulatedMonth);
+
+// расчет кол-ва месяцев для достижения фин. цели 
+let missionMonthCount = getTargetMonth(mission, accumulatedMonth);
+if (missionMonthCount > 0) {
+    console.log('Достижение цели', mission,'за', Math.floor(missionMonthCount), 'мес.');
+} else {
+    console.log('Цель не будет достигнута');
+}
+
+// вычислим бюджет на день и выведем сообщение при отрицательном бюджете
+let budgetDay = getBudgetDay(accumulatedMonth);
+if ( budgetDay < 0) {
+    console.log(getStatusIncome(budgetDay));
+};
+
+
