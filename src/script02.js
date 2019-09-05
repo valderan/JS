c0f34d88ffc4a7d48c9ef0041626e41431dac5ce
git <v.kundryukov@gmail.com>
2019-09-05 17:16:57 +0300

    lesson 07 hard

--- src/script02.js
+++ src/script02.js
@@ -1,40 +1,47 @@
 'use strict';
 
-let week = [
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота',
-    'Воскресенье'
-];
-
 let date = new Date();
-let weekDayIndex = date.getDay();
 
-( weekDayIndex === 0 ) ? weekDayIndex = 7 : weekDayIndex++; 
-
-let stringHTML = '';
-week.forEach((element, index) => {
-    let html = '';
-    
-    if (index > 4) {
-        html = '<i>' + element + '</i>';
-    } else {
-        html = element;
+// вывод в body
+let out = function (string = '', br = true, position = 'beforeend') {
+    if (br) {
+        string += '<br>';
     }
+    document.body.insertAdjacentHTML(position, string);
+};
 
-    if (weekDayIndex === (index + 1)) {
-        html = '<b>' + html + '</b>';
-    };
+let htmlString = date.toLocaleString().split(',')[1] + ' ' 
+                + date.toLocaleString().split(',')[0];
+          
+out(htmlString);
 
-    stringHTML += html + '<br>';
-});
+// проверка времени
+let checkTime = function(tString) {
+    
+    let tArr = tString.split(' '),
+        hms = tArr[0].split(':'),
+        dms = tArr[1].split('.');
+    
+        hms.forEach((element, index) => {
 
-let div = document.createElement('div');
-div.innerHTML = stringHTML;
+            if (Number(element) < 10 ) {
+                hms[index] = '0' + hms[index]
+            }
 
-document.body.append(div);
+        });
 
+        dms.forEach((element, index) => {
 
+            if (Number(element) < 10 ) {
+                dms[index] = '0' + dms[index]
+            }
+
+        });
+        
+        let hmsString = hms[0] + ':' + hms[1] + ':' + hms[2],
+            dmsString = dms[0] + '.' + dms[1] + '.' + dms[2]    
+         
+        return hmsString + ' ' + dmsString;
+};
+
+out(checkTime('9:5:3 1.6.2019'));
