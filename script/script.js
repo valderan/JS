d9f488ec36f9131a6509b6971e9b725b130015bd
git <v.kundryukov@gmail.com>
2019-09-25 21:46:51 +0300

    lesson 15 base

--- script/script.js
+++ script/script.js
@@ -354,4 +354,57 @@ window.addEventListener('DOMContentLoaded', function() {
     };
 
     slider();
+
+
+    // замена фото команды на новое при наведении и возврат старого фото, при уводе мышки с фото
+    const imgReplacement = () => {
+
+        const photo = Array.from(document.querySelectorAll('.command__photo')),
+            command = document.getElementById('command');
+
+        
+        const reverseAttr = (target) => {
+            let dataImg = target.getAttribute('data-img'),
+                srcImg =  target.getAttribute('src');
+                target.setAttribute('src', dataImg);
+                target.setAttribute('data-img', srcImg);
+        };
+        
+        command.addEventListener('mouseover', (event) => {
+            let target = event.target;    
+            if (target.matches('.command__photo')) {
+                reverseAttr(target);
+            }
+        });
+
+        command.addEventListener('mouseout', (event) => {
+            let target = event.target;    
+            if (target.matches('.command__photo')) {
+                reverseAttr(target);
+            }
+        });
+
+
+    };
+
+    imgReplacement();
+
+// проверка значений правильности ввода калькулятора, только цифры
+function checkInputValid() {
+
+    let dataDiv = Array.from(document.querySelector('.calc-block').getElementsByTagName('input'));
+    
+    dataDiv.forEach((element) => {
+        element.addEventListener('input', ()=> {
+            element.value = element.value.replace(/[^\d.1]/g,'');
+        });   
+    });
+
+}
+
+checkInputValid();
+
+
 });
+
+
